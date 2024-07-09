9bd8667d932234cffdfc9af147afba5dde20485b
david-parkk <ji0513ji@naver.com>
2024-07-09 20:55:12 +0900

    init

---
+++ src/main/java/com/kutaverse/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.kutaverse.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
